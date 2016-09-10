33d3779ccc85225ee96ca809c8e9e8880b0d9685
Lior Deutsch <sliorde@gmail.com>
2016-09-10 18:02:46 +0300

    added option to display clustering graphically

--- python/QC.py
+++ python/QC.py
@@ -1,21 +1,20 @@
 import numpy as np
 import cluster
-# import ipdb
-# ipdb.set_trace()
 
-
-data = np.loadtxt('..\Iris.csv',delimiter=',')
+data = np.loadtxt('Iris.csv',delimiter=',')
 data = data[:,:4]
 
-sigma=0.5
-repetitions=50
-stepSize=0.15
+sigma=0.55
+repetitions=100
+stepSize=0.1
 clusteringType='v'
 recalculate=False
 returnHistory=True
 stopCondition=True
-voxelSize = 0.3
+voxelSize = None
 
 x,xHistory = cluster.GradientDescent(data,sigma=sigma,repetitions=repetitions,stepSize=stepSize,clusteringType=clusteringType,recalculate=recalculate,returnHistory=returnHistory,stopCondition=stopCondition,voxelSize=voxelSize)
 
 clusters = cluster.PerformFinalClustering(x,stepSize)
+
+cluster.displayClustering(xHistory,clusters)
